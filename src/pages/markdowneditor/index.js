5494ff4e64a35221844ec74cc04a748ba1defd69
andrewquang <andrew.viquang@gmail.com>
2021-07-14 23:42:11 +0700

    make title

--- src/pages/markdowneditor/index.js
+++ src/pages/markdowneditor/index.js
@@ -3,7 +3,11 @@ import Styles from '../../assets/pomodoroclock/styles';
 import '../../assets/main.css'
 
 const Markdowneditor = () =>{
-    
+    return (
+        <div>
+            <h1>WELCOME TO MY MARKDOWNEDITOR</h1>
+        </div>
+    );
 };
 
 export default Markdowneditor;
