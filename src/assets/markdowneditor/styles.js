f5932afd4186eb23ee1f8aa66f9921eda7c3151f
andrewquang <andrew.viquang@gmail.com>
2021-07-15 00:07:41 +0700

    update some initial files on markdowneditor

---
+++ src/assets/markdowneditor/styles.js
@@ -0,0 +1,81 @@
+const Styles = {
+    container: [
+        'w-full',
+        'h-full',
+        'px-8',
+        'py-12',
+        'sm:px-5',
+        'sm:py-24',
+        'mx-auto',
+        'bg-green-300',
+    ].join(' '),
+
+    main: [
+        'flex',
+        'flex-col',
+        'text-center',
+        'w-full',
+        'sm:mb-12',
+    ].join(' '),
+
+    title:[
+        'text-3xl',
+        'h-full',
+        'w-full',
+        'font-medium',
+        'title-font',
+        'text-gray-900',
+        'sm:text-6xl',
+        'sm:mb-4',
+        'sm:h-auto',
+    ].join(' '),
+
+    time:[
+        'mt-8',
+        'mb-8',
+        'text-7xl',
+        'mx-auto',
+        'leading-relaxed',
+        'text-base',
+        'sm:text-8xl',
+    ].join(' '),
+
+    controller:[
+        'flex',
+        'w-9/12',
+        'flex-col',
+        'sm:flex-row',
+        'mx-auto',
+        'px-8',
+        'sm:space-x-4',
+        'sm:space-y-0',
+        'space-y-4',
+        'sm:px-0',
+        'items-end',
+    ].join(' '),
+
+    center_flex_container:[
+        'flex',
+        'relative',
+        'flex-grow',
+        'w-full',
+        'justify-center',
+        'items-center',
+    ].join(' '),
+    button:[
+        'px-6',
+        'py-6',
+        'w-full',
+        'sm:w-3/6',
+        'text-2xl',
+        'text-white',
+        'bg-indigo-500',
+        'border-0',
+        'focus:outline-none',
+        'hover:bg-indigo-600',
+        'rounded',
+        'sm:px-3',
+    ].join(' '),
+}
+
+export default Styles
--- src/pages/markdowneditor/index.js
+++ src/pages/markdowneditor/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Styles from '../../assets/pomodoroclock/styles';
+import Styles from '../../assets/markdowneditor/styles';
 import '../../assets/main.css'
 
 const Markdowneditor = () =>{
     return (
-        <div>
-            <h1>WELCOME TO MY MARKDOWNEDITOR</h1>
+        <div className={Styles.container}>
+            <div className={Styles.introdution}>
+                <h1>WELCOME TO MY MARKDOWNEDITOR</h1>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div className={Styles.main}>
+                <div className={Styles.leftcontent}>
+
+                </div>
+                <div className={Styles.rightcontent}>
+                    
+                </div>
+            </div>
         </div>
     );
 };
