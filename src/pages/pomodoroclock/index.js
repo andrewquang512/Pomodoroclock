da4597ad02bf022393a6731383d272816de42c09
andrewquang512 <andrew.viquang@gmail.com>
2022-03-16 21:04:40 +0700

    fix for deploy

--- src/pages/pomodoroclock/index.js
+++ src/pages/pomodoroclock/index.js
@@ -1,115 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import Styles from '../../assets/pomodoroclock/styles';
-import '../../assets/main.css'
+import '../../assets/main.css';
 
-const Pomodoroclock = () =>{
-    const [BreakLength, setBreakLength] = useState(60 * 5);
-    const [SessionLength, setSessionLength] = useState(60 * 25);
-    const [TimerLeft, setTimerLeft] = useState(SessionLength);
-    const [Status, setStatus] = useState('New');
-    const [Type, setType] = useState('Session');
-    const [audio] = useState(new Audio('https://www.soundjay.com/clock/sounds/alarm-clock-01.mp3'));
-    const [playing, setPlaying] = useState(false);
+const Pomodoroclock = () => {
+  const BreakLength = 60 * 5;
+  const SessionLength = 60 * 25;
+  const [TimerLeft, setTimerLeft] = useState(SessionLength);
+  const [Status, setStatus] = useState('New');
+  const [Type, setType] = useState('Session');
+  const [audio] = useState(
+    new Audio('https://www.soundjay.com/clock/sounds/alarm-clock-01.mp3')
+  );
+  const [playing, setPlaying] = useState(false);
 
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-      },
-      [playing,audio]
-    );
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing, audio]);
 
-    useEffect(() => {
-      audio.addEventListener('ended', () => setPlaying(false));
-      return () => {
-        audio.removeEventListener('ended', () => setPlaying(false));
-      };
-    }, []);
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, [audio]);
 
-    const Timedisplay = (time) => {
-      let minute = Math.floor(time/60);
-      let second = time - minute * 60;
-      if(minute < 10)  minute = "0" + minute;
-      if(second < 10)  second = "0" + second;
-      return minute + ":" + second;
+  const Timedisplay = (time) => {
+    let minute = Math.floor(time / 60);
+    let second = time - minute * 60;
+    if (minute < 10) minute = '0' + minute;
+    if (second < 10) second = '0' + second;
+    return minute + ':' + second;
+  };
+
+  useEffect(() => {
+    var timerID;
+    if (Status === 'Running') {
+      timerID = setInterval(() => {
+        const newTimerLeft = TimerLeft - 1;
+        if (newTimerLeft >= 0) {
+          if (Type === 'Session' && newTimerLeft === 60 * 25 - 10)
+            setPlaying(false);
+          if (Type === 'Break' && newTimerLeft === 60 * 5 - 10)
+            setPlaying(false);
+          setTimerLeft(TimerLeft - 1);
+        } else {
+          setPlaying(true);
+          if (Type === 'Session') {
+            setTimerLeft(BreakLength);
+            setType('Break');
+          } else {
+            setTimerLeft(SessionLength);
+            setType('Session');
+          }
+        }
+      }, 1000);
+    }
+    if (Status === 'Stop') {
+      return;
     }
 
-    useEffect(() => {
-      var timerID;
-      if(Status === 'Running'){
-        timerID = setInterval(
-          () =>{
-            const newTimerLeft = TimerLeft - 1;
-            if (newTimerLeft >= 0){
-              if(Type === 'Session' && newTimerLeft === (60 * 25 - 10) ) setPlaying(false);
-              if(Type === 'Break' && newTimerLeft === (60 * 5 - 10) ) setPlaying(false);
-              setTimerLeft(TimerLeft - 1);
-            } 
-            else {
-              setPlaying(true);
-              if(Type === 'Session'){
-                setTimerLeft(BreakLength);
-                setType('Break');
-              }
-              else{
-                setTimerLeft(SessionLength);
-                setType('Session');
-              }
-            }
-          }, 1000 
-        );
-      };
-      if(Status === 'Stop') {
-         return ;
-      };
-      
-      return () => clearInterval(timerID);
-    }, [Status, TimerLeft, Type , BreakLength, SessionLength]);
+    return () => clearInterval(timerID);
+  }, [Status, TimerLeft, Type, BreakLength, SessionLength]);
 
+  const handleReset = () => {
+    setTimerLeft(SessionLength);
+    setType('Session');
+    setStatus('New');
+  };
 
-
-    const handleReset = () => {
-      setTimerLeft(SessionLength);
-      setType('Session');
-      setStatus('New');
-    }
-
-    const handleStartStop = () => {
-      if(Status === 'New') setStatus('Running');
-      if(Status === 'Running') setStatus('Stop');
-      if(Status === 'Stop') setStatus('Running');
-    }
-    //TODO: it should have a break status
-    return (
+  const handleStartStop = () => {
+    if (Status === 'New') setStatus('Running');
+    if (Status === 'Running') setStatus('Stop');
+    if (Status === 'Stop') setStatus('Running');
+  };
+  //TODO: it should have a break status
+  return (
     <div className={Styles.container}>
       <div className={Styles.main}>
         <h1 className={Styles.title}>
-          { (Status === 'New' && Type === 'Session') ? "Can you focus in 25 minutes?"
-            : (Status === 'Running' && Type === 'Session') ? "You're doing great, keep up"
-            : (Status === 'Stop' && Type === 'Session') ? "Just a little more to done, try hard"
-            : "You are awesome, it's time for break"} 
+          {Status === 'New' && Type === 'Session'
+            ? 'Can you focus in 25 minutes?'
+            : Status === 'Running' && Type === 'Session'
+            ? "You're doing great, keep up"
+            : Status === 'Stop' && Type === 'Session'
+            ? 'Just a little more to done, try hard'
+            : "You are awesome, it's time for break"}
         </h1>
-        <h1 className={Styles.time}>
-          {Timedisplay(TimerLeft)}
-        </h1>
+        <h1 className={Styles.time}>{Timedisplay(TimerLeft)}</h1>
       </div>
       <div className={Styles.controller}>
         <div className={Styles.center_flex_container}>
-          <button className={Styles.button}
-          onClick={handleStartStop}>
-          { (Status === 'New') ? "Start"
-            : (Status === 'Running') ? "Stop"
-            : (Status === 'Stop') ? "Resume"
-            : "Start"}
+          <button className={Styles.button} onClick={handleStartStop}>
+            {Status === 'New'
+              ? 'Start'
+              : Status === 'Running'
+              ? 'Stop'
+              : Status === 'Stop'
+              ? 'Resume'
+              : 'Start'}
           </button>
         </div>
         <div className={Styles.center_flex_container}>
-          <button className={Styles.button}
-          onClick={handleReset}>
+          <button className={Styles.button} onClick={handleReset}>
             Reset
           </button>
         </div>
+      </div>
     </div>
-  </div>
-    );
+  );
 };
 
 export default Pomodoroclock;
